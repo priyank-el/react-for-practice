ea0b9c91307d3ff045ba009222ff30f5113be005
priyankambliya <priyankambliya18@gmail.com>
2023-10-20 23:03:11 +0530

    fix:modify file

--- src/components/pages/AllDataPage.jsx
+++ src/components/pages/AllDataPage.jsx
@@ -1,58 +1,59 @@
-import { Table } from "antd"
-import data from '../../data/totalData'
+import axios from "axios";
 import { useEffect, useState } from "react";
-import { Navigate } from "react-router-dom";
 function AllDataPage() {
 
-  const [array,setArray] = useState([])
+  const [ totalData,setTotaldata ] = useState([])
+  
+  useEffect(()=>{
+    const fetchData = async () => {
+      debugger
+      try {
+        const {data} = await axios.get("http://localhost:3000/data")
+        if(data) setTotaldata(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetchData()
+  },[setTotaldata])
 
-  const columns = [
-    {
-      title: 'Username',
-      dataIndex: 'username',
-      key: 'username',
-    },
-    {
-      title: 'Email',
-      dataIndex: 'email',
-      key: 'email',
-    },
-    {
-      title: 'Password',
-      dataIndex: 'password',
-      key: 'password',
-    },
-    {
-      title: 'Action',
-      key: 'action',
-      render: (_) => (
-        <a onClick={() => {
-          console.log(_);
-          const index = data
-            .map(function (element) {
-              return element.email;
-            })
-            .indexOf(_.email);
-          data.splice(index, 1)
-          console.log(data);
-          setArray(data)
-        }}>Delete</a>
-      ),
-    },
-  ];
-    useEffect(()=>{
-      let dataSource = data.map((data, index) => {
-        return {
-          ...data,
-          key: index + 1
-        }
-      })
-      setArray(dataSource)
-    },[setArray])
-   
+  const handleDeleteEvent = async () => {
+    debugger
+    const {data} = await axios.delete
+    (`http://localhost:3000/data/`)
+    if(data) console.log(totalData)
+    setTotaldata(totalData)
+  }
 
   return (
-    <Table dataSource={array} columns={columns} />
+    <>
+      <div>
+        <h1 className="text-center text-3xl font-extrabold">All Data Page</h1>
+        <div className=" flex justify-center">
+          <table className="w-3/4 mt-10">
+            <tr>
+              <th className="border border-1 border-black">Name</th>
+              <th className="border border-1 border-black">Email</th>
+              <th className="border border-1 border-black">Password</th>
+              <th className="border border-1 border-black">Delete</th>
+            </tr>
+            {
+              totalData.map( user => (
+                <tr>
+                  <td className="text-center border border-1 border-black">{user.name}</td>
+                  <td className="text-center border border-1 border-black">{user.email}</td>
+                  <td className="text-center border border-1 border-black">{user.password}</td>
+                  <td className="text-center border border-1 border-black">
+                    <button onClick={handleDeleteEvent}>delete</button>
+                  </td>
+                </tr>
+              ) )
+            }
+          </table>
+            {/* <button type="submit" className="bg-teal-600 p-1 rounded-xl text-white px-2">Delete</button> */}
+        </div>
+      </div>
+    </>
   )
 }
 
